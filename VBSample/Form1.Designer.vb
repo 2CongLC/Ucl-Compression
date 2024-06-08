69a3164c2d9874ff958a587f91c5e1690bb9af59
ChuCong <2conglc.vn@gmail.com>
2024-06-08 11:47:42 +0700

    update

--- VBSample/Form1.Designer.vb
+++ VBSample/Form1.Designer.vb
@@ -28,24 +28,26 @@ Partial Class Form1
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(38, 154)
+        Me.Button1.Location = New System.Drawing.Point(65, 54)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(136, 36)
+        Me.Button1.Size = New System.Drawing.Size(236, 36)
         Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Mã hóa"
+        Me.Button1.Text = "Mã hóa  NRV2B_99"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(180, 154)
+        Me.Button2.Location = New System.Drawing.Point(65, 96)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(138, 36)
+        Me.Button2.Size = New System.Drawing.Size(236, 36)
         Me.Button2.TabIndex = 1
-        Me.Button2.Text = "Giải mã"
+        Me.Button2.Text = "Mã Hóa NRV2D_99"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'LinkLabel1
@@ -58,11 +60,31 @@ Partial Class Form1
         Me.LinkLabel1.TabStop = True
         Me.LinkLabel1.Text = "https://2conglc-vn.blogspot.com/"
         '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(65, 138)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(236, 36)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Mã Hóa NRV2E_99"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(65, 209)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(236, 23)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "Test"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(539, 437)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.LinkLabel1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
@@ -81,4 +103,6 @@ Partial Class Form1
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
 End Class
--- VBSample/Form1.vb
+++ VBSample/Form1.vb
@@ -5,6 +5,11 @@ Imports UclCompression
 
 
 Public Class Form1
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("https://2conglc-vn.blogspot.com/")
+    End Sub
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
@@ -22,16 +27,31 @@ Public Class Form1
 
         If OpenFileDialog1.ShowDialog = DialogResult.OK AndAlso SaveFileDialog1.ShowDialog = DialogResult.OK Then
             Dim buffer As Byte() = IO.File.ReadAllBytes(OpenFileDialog1.FileName)
-            Dim Uncompressed As Byte() = Ucl.NRV2B_Decompress_8(buffer, buffer.Length)
-            IO.File.WriteAllBytes(SaveFileDialog1.FileName, Uncompressed)
+            Dim compressed As Byte() = Ucl.NRV2D_99_Compress(buffer, 10)
+            IO.File.WriteAllBytes(SaveFileDialog1.FileName, compressed)
             MessageBox.Show("Đã xong !")
         End If
     End Sub
 
 
-    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
-        Process.Start("https://2conglc-vn.blogspot.com/")
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        If OpenFileDialog1.ShowDialog = DialogResult.OK AndAlso SaveFileDialog1.ShowDialog = DialogResult.OK Then
+            Dim buffer As Byte() = IO.File.ReadAllBytes(OpenFileDialog1.FileName)
+            Dim compressed As Byte() = Ucl.NRV2E_99_Compress(buffer, 10)
+            IO.File.WriteAllBytes(SaveFileDialog1.FileName, compressed)
+            MessageBox.Show("Đã xong !")
+        End If
     End Sub
 
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
 
+        If OpenFileDialog1.ShowDialog = DialogResult.OK AndAlso SaveFileDialog1.ShowDialog = DialogResult.OK Then
+            Dim buffer As Byte() = Encoding.ASCII.GetBytes("123abc")
+            Dim compressed As Byte() = Ucl.NRV2B_99_Compress(buffer, 10)
+            Dim uncompressed As Byte() = Ucl.NRV2B_99_Compress(compressed, buffer.Length)
+
+            MessageBox.Show("Đã xong !")
+        End If
+    End Sub
 End Class
